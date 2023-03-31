8ea70d5930a9a8229f30451da53621f6e50720f1
joaoramirezz <joao.rkonrath@gmail.com>
2023-03-31 11:42:15 -0300

    segundo commit

--- IACode.py
+++ IACode.py
@@ -1,19 +1,24 @@
-boloCenoura = ['farinha','trigo','ovo','cenoura','fermento','bolo de cenoura']
-boloMorango = ['farinha','trigo','ovo','morango','fermento','bolo de morango']
-boloChocolate = ['farinha','trigo','ovo','chocolate','fermento','bolo de chocolate']
-boloMaracuja = ['farinha','trigo','ovo','maracuja','fermento','bolo de maracuja']
+boloCenoura = ['farinha','trigo','ovo','cenoura','fermento']
+boloMorango = ['farinha','trigo','ovo','morango','fermento']
+boloChocolate = ['farinha','trigo','ovo','chocolate','fermento']
+boloMaracuja = ['farinha','trigo','ovo','maracuja','fermento']
 
-pudimDeLeite = ['leite condensado','leite','ovo','pudim de leite']
-pudimDePao = ['leite condensado','leite','ovo','pão','pudim de pão']
+pudimDeLeite = ['leite condensado','leite','ovo']
+pudimDePao = ['leite condensado','leite','ovo']
 
-TortaLimao = ['maizena','leite condensado','limão','glace','torta de limão']
-TortaBanana = ['maizena','leite condensado','banana','glace','torta de banana']
+TortaLimao = ['maizena','leite condensado','limão','glace']
+TortaBanana = ['maizena','leite condensado','banana','glace']
 
-brigadeiro = ['chocolate','leite condensado','manteiga','brigadeiro']
-beijinho = ['baunilha','leite condensado','manteiga','coco','beijinho']
+brigadeiro = ['chocolate','leite condensado','manteiga']
+beijinho = ['baunilha','leite condensado','manteiga','coco']
 
 
-doces = [boloCenoura,boloMorango,boloChocolate,boloMaracuja,pudimDeLeite,pudimDePao,TortaLimao,TortaBanana,brigadeiro,beijinho]
+doces = {'bolo de cenoura' : boloCenoura , 'bolo de morango' : boloMorango, 
+         'bolo de chocolate' : boloChocolate, 'bolo de maracuja' : boloMaracuja,
+         'pudim de leite' : pudimDeLeite, 'pudim de pão' : pudimDePao,
+         'torta de limão' : TortaLimao, 'torta de banana' : TortaBanana,
+         'brigadeiro' : brigadeiro, 'beijinho' : beijinho
+         }
 
 
 
@@ -22,11 +27,22 @@ def VerificarSobremesa(Lista):
     global doces
     docespossiveis = []
     
-    for ingrediente in Lista:
+    for ig in Lista:
         for doce in doces:
-            for ingredienteDoce in doce:
-                if ingrediente == ingredienteDoce:
-                    docespossiveis.append(doce[-1])
+            for ig2 in doces[doce]:
+                if ig != ig2:
+                    print(ig + '/' + ig2 + 'DIFERENTE')
+                    break
+            docespossiveis.append(doces)
+        
+                
+    
+    
+    # for ingrediente in Lista:
+    #     for doce in doces:
+    #         for ingredienteDoce in doce:
+    #             if ingrediente == ingredienteDoce:
+    #                 docespossiveis.append(doce[-1])
                     
     
     
